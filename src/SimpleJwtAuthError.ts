752750a550ae5421e578f41982273ea797b93d3c
Ulad Kasach <uladkasach@gmail.com>
2023-02-23 10:12:22 -0500

    fix(practs): upgrade to latest declapract-typescript-ehmpathy best practices

--- src/SimpleJwtAuthError.ts
+++
@@ -1,1 +0,0 @@
-export class SimpleJwtAuthError extends Error {} // just give it a class so that folks can check `error instanceof SimpleJwtAuthError` or something else
---
+++ src/utils/errors/SimpleHmacKeyAuthError.ts
@@ -0,0 +1,1 @@
+export class SimpleHmacKeyAuthError extends Error {} // just give it a class so that folks can check `error instanceof SimpleHmacKeyAuthError`
