4131df20d68508e781abed57a6be49d1b4f8caaf
Ulad Kasach <uladkasach@gmail.com>
2023-02-24 04:50:50 -0500

    fix(contract): expose two utilities for public use

--- src/contract/index.ts
+++ src/contract/index.ts
@@ -4,6 +4,10 @@ export { createSecureRequestSignature } from '../logic/signatures/createSecureRe
 export { getRequestSignatureFromHeaders } from '../logic/headers/getRequestSignatureFromHeaders';
 export { issueClientKeyPair } from '../logic/keys/issueClientKeyPair';
 
+// utilities
+export { decodeRequestSignatureMetadata } from '../logic/signatures/decodeRequestSignatureMetadata';
+export { isRequestSignature } from '../logic/signatures/isRequestSignature';
+
 // errors
 export { UnauthenticRequestSignatureError } from '../utils/errors/UnauthenticRequestSignatureError';
 export { UnauthableRequestSignatureError } from '../utils/errors/UnauthableRequestSignatureError';
